04e0a5741a22647f4beb08b4cb26fea7f10fae2e
Artem Magei <artemmagej@gmail.com>
2024-02-18 17:57:05 +0200

    feat: create forecast item

---
+++ src/components/ForecastItem/ForecastItem.tsx
@@ -0,0 +1,30 @@
+import "./ForecastItem.css";
+
+import { format } from "date-fns";
+import React from "react";
+
+import { ForecastPerDayType } from "../../api/forecast/forecast.types";
+
+export type ForecastItemProps = {
+  day: ForecastPerDayType;
+};
+
+export const ForecastItem = ({ day }: ForecastItemProps) => {
+  const getWeekday = (date: string) => {
+    return format(new Date(date), "EEEE");
+  };
+
+  return (
+    <div className="forecast-item-container">
+      <span className="forecast-item-weekday">{getWeekday(day.datetime)}</span>
+      <img
+        loading="lazy"
+        src={`/assets/forecast/${day.icon}.svg`}
+        alt={day.icon}
+      />
+      <span>
+        {Math.round(day.tempmax)}°/{Math.round(day.tempmin)}°
+      </span>
+    </div>
+  );
+};
