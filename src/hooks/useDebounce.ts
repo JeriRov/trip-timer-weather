a6e7fc5e4a922b8cf668815a9fc8fc8b335fc123
Artem Magei <artemmagej@gmail.com>
2024-02-21 15:30:15 +0200

    feat: create hook instead debounce util

---
+++ src/hooks/useDebounce.ts
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+type Timer = ReturnType<typeof setTimeout>;
+type DelayFunction<T> = (...args: T[]) => void;
+export const useDebounce = <T>(func: DelayFunction<T>, delay: number) => {
+  const [timer, setTimer] = useState<Timer>();
+  return ((...args) => {
+    const newTimer = setTimeout(() => {
+      func(...args);
+    }, delay);
+    clearTimeout(timer);
+    setTimer(newTimer);
+  }) as DelayFunction<T>;
+};
--- src/utils/debounce.ts
+++
@@ -1,8 +0,0 @@
-type DelayFunction<T> = (...args: T[]) => void;
-export const debounce = <T>(func: DelayFunction<T>, delay: number) => {
-  let timeoutId: NodeJS.Timeout;
-  return (...args: Parameters<DelayFunction<T>>) => {
-    clearTimeout(timeoutId);
-    timeoutId = setTimeout(() => func(...args), delay);
-  };
-};
